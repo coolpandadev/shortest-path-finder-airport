7a2692fba4ae709dc687e159e2deb28ba70a287e
coolpandadev <coolpanda1219@gmail.com>
2022-03-12 15:26:39 +0100

    added custom selects

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import './App.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Select from './components/Select'
 import { airports, bfs } from './bfs'
 function App() {
-	const [startLoc, setStartLoc] = useState(airports[0])
-	const [endLoc, setEndLoc] = useState(airports[1])
+	const [startLoc, setStartLoc] = useState()
+	const [endLoc, setEndLoc] = useState()
 	const [path, setPath] = useState('Tutaj pojawi się Twoja trasa')
-	const handleStartLocChange = e => setStartLoc(e.target.value)
-	const onClick = () => {
-		let bfsOutput = bfs(startLoc, endLoc)
-		if (typeof bfsOutput !== 'string') {
-			for (let i = 0; i < bfsOutput.length - 1; i++) {
-				bfsOutput[i] += ' ==> '
-			}
-		}
-		setPath(bfsOutput)
-	}
 
-	const handleEndLocChange = e => {
-		setEndLoc(e.target.value)
+	const submitStartAirportChange = e => {
+		setStartLoc(e.target.textContent)
 	}
-	const showPath = second => {}
+	const submitEndAirportChange = e => {
+		setEndLoc(e.target.textContent)
+	}
+	useEffect(() => {
+		setPath(bfs(startLoc, endLoc))
+	}, [startLoc, endLoc])
+
 	return (
 		<div className='container'>
 			<h1 className='header'>Shortest Path Finder</h1>
-			<Select value={startLoc} options={airports} onChange={handleStartLocChange} />
-			<Select value={endLoc} options={airports} onChange={handleEndLocChange} />
-			<button className='button' onClick={onClick}>
-				Pokaż najkrótszą drogę
-			</button>
+
+			<Select options={airports} onClick={submitStartAirportChange} currentAirport={startLoc} />
+			<Select options={airports} onClick={submitEndAirportChange} currentAirport={endLoc} />
+
 			<p>{`Najszybsza droga z lotniska ${startLoc} do lotniska ${endLoc} to:  `}</p>
 			<p>{path}</p>
 		</div>
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,16 +1,32 @@
-import { computeHeadingLevel } from '@testing-library/react'
+import '../sass/custom-select.scss'
+import { useState } from 'react'
 import React from 'react'
-const Select = ({ value, options, onChange }) => {
+const Select = ({ options, onClick, currentAirport }) => {
+	const [showDropDownMenu, setShowDropDownMenu] = useState(false)
+	const toggleDropDownMenu = () => {
+		setShowDropDownMenu(!showDropDownMenu)
+	}
 	return (
-		<select className='select' value={value} onChange={onChange}>
-			{options.map(option => {
-				return (
-					<option className='select__option' key={option} value={option}>
-						{option}
-					</option>
-				)
-			})}
-		</select>
+		<div className='custom-select'>
+			<button className='select-btn' onClick={toggleDropDownMenu}>
+				{currentAirport || 'Wybierz lotnisko'}
+			</button>
+			<div className='options-container' style={showDropDownMenu ? { display: 'flex' } : { display: 'none' }}>
+				{options.map(option => {
+					return (
+						<p
+							key={option}
+							className='option'
+							onClick={e => {
+								onClick(e)
+								toggleDropDownMenu()
+							}}>
+							{option}
+						</p>
+					)
+				})}
+			</div>
+		</div>
 	)
 }
 
